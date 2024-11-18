b71482a043efdd671a7d3433cc60f7dd45252899
Amelia Vasiliu <ega9cw@virginia.edu>
2024-11-17 19:35:08 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,6 +12,9 @@ DBUSER = "admin"
 DBPASS = os.getenv('DBPASS')
 DB = "ega9cw"
 
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
+
 app = FastAPI()
 from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
@@ -20,25 +23,31 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-@app.get("/")  # zone apex
-def zone_apex():
-    return {"Hello": "Hello API!"}
 
-@app.get("/sum/{a}/{b}")
-def add(a: int, b: int):
-    return {"sum": a + b}
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
 
-@app.get("/multiply/{c}/{d}")
-def multiply(c: int, d: int):
-    return {"product": c * d}
-
-@app.get("/square/{a}")
-def square(a: int):
-    return {"square": a ** 2}
-
-@app.get("/divide/{c}/{d}")
-def divide(c: int, d: int):
-    return {"divide": c / d}
-
-db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
-cur=db.cursor()
+@app.get('/songs')
+def get_songs():
+    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, genres.genre FROM `songs` JOIN genres ON genres.genreid = songs.genre;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
